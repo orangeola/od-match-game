a989b6943225a8a772cbaddf2af87c13ee612cdf
orangeola <orangeqmm@gmail.com>
2023-02-03 18:30:09 +1030

    Add skeleton files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <p>High Score: 0</p>
+      <p>Current Score: 0</p>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Main = () => {
+  const [cardArray, setCardArray] = useState([]);
+
+  return (
+    <div className="Main">
+      <p>Show clickable cards here</p>
+    </div>
+  );
+};
+
+export default Main;
